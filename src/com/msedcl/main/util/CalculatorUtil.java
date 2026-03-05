61c4355d40b80a311973edd2eeaf38011fc51daa
student17 <student17@host.docker.internal>
2026-03-05 12:15:55 +0530

    Change is subtraction

--- src/com/msedcl/main/util/CalculatorUtil.java
+++ src/com/msedcl/main/util/CalculatorUtil.java
@@ -5,8 +5,10 @@ public class CalculatorUtil {
 		return n1 + n2;
 	}
 	
-	public int subtraction(int n1, int n2) {
-		return n1 - n2;
+	public int subtraction(int num1, int num2) {
+		System.out.println("Subtraction--------");
+		
+		return num1 - num2;
 	}
 	
 	public int mulultiplication(int n1, int n2) {
