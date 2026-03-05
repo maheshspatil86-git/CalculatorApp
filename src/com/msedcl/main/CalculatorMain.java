abb4f943fc4d79928a5756c881e7ea43f5e74fcc
student16 <student16@PC16.mshome.net>
2026-03-05 11:22:46 +0530

    subtraction feature

--- src/com/msedcl/main/CalculatorMain.java
+++ src/com/msedcl/main/CalculatorMain.java
@@ -6,6 +6,6 @@ public class CalculatorMain {
 	public static void main(String[] args) {
 		CalculatorUtil cu = new CalculatorUtil();
 		System.out.println("Addition: "+cu.addition(10, 20));
+		System.out.println("Subtraction: "+cu.subtraction(10, 20));
 	}
-
 }
